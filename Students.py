c247008fce4929ebdc40f8dcb91fba97f85ad424
mhmdjawad1 <baghdadijawad74@gmail.com>
2025-06-23 17:24:31 -0700

    Creating dictionary for students

---
+++ Students.py
@@ -0,0 +1,13 @@
+records = [
+    ["Layla", 89], ["Tariq", 77], ["Layla", 91], ["Jana", 100], ["Tariq", 84],
+    ["Ziad", 62], ["Jana", 97], ["Tariq", 73], ["Ziad", 71], ["Layla", 86],
+    ["Jana", 94], ["Ziad", 75]
+]
+students = {}
+
+
+
+for name, grade in records:
+    students[name] = students.get(name,[]) + [grade]
+
+print(students)
